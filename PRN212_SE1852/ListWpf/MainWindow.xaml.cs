9686f33db27cf01c3b1d8248a57a0ddd5d08e975
Nguyễn Gia Huy <huyngse184439@fpt.edu.vn>
2025-06-17 22:52:52 +0700

    .

--- PRN212_SE1852/ListWpf/MainWindow.xaml.cs
+++ PRN212_SE1852/ListWpf/MainWindow.xaml.cs
@@ -24,14 +24,11 @@ namespace ListWpf
 
         private void BtnThem_Click(object sender, RoutedEventArgs e)
         {
-            //x là giá trị muốn đưa vào cuối danh sách 
             int x = int.Parse(txtGiaTri.Text);
-            //thêm x vào danh sách: 
             dsDuLieu.Add(x);
             HienThiDanhDanh();
             txtGiaTri.Text = "";
         }
-        //Hàm hiển thị danh sách lên giao diện 
         void HienThiDanhDanh()
         {
             lstDuLieu.Items.Clear();
@@ -43,32 +40,22 @@ namespace ListWpf
         }
         private void BtnChen_Click(object sender, RoutedEventArgs e)
         {
-            //x là giá trị muốn chèn 
             int x = int.Parse(txtGiaTriChen.Text);
-            //vt mà ta chèn x vào 
             int vt = int.Parse(txtViTriChen.Text);
-            //chèn x vào vị trí vt 
             dsDuLieu.Insert(vt, x);
-            //hiển thị lại danh sách 
             HienThiDanhDanh();
             txtViTriChen.Text = "";
             txtGiaTriChen.Text = "";
         }
         private void BtnSapXepTang_Click(object sender, RoutedEventArgs e)
         {
-            //gọi lệnh sắp xếp danh sách: 
             dsDuLieu.Sort();
-            //hiển thị lại danh sách 
             HienThiDanhDanh();
         }
-        private void BtnSapXepGiam_Click(object sender,
-                                RoutedEventArgs e)
+        private void BtnSapXepGiam_Click(object sender,RoutedEventArgs e)
         {
-            //sắp xếp tăng dần 
             dsDuLieu.Sort();
-            //đảo lại danh sách 
             dsDuLieu.Reverse();
-            //hiển thị lại danh sách 
             HienThiDanhDanh();
         }
         private void BtnXoa1PhanTu_Click(object sender, RoutedEventArgs e)
@@ -78,7 +65,6 @@ namespace ListWpf
                 MessageBox.Show("Phải chọn phần tử mới xóa được", "Thông báo lỗi", MessageBoxButton.OK); 
                 return;
             }
-            //xóa phần tử tại vị trí đang chọn:  
             dsDuLieu.RemoveAt(lstDuLieu.SelectedIndex);
             HienThiDanhDanh();
         }
@@ -90,21 +76,16 @@ namespace ListWpf
                 MessageBox.Show("Phải chọn phần tử mới xóa được", "Thông báo lỗi", MessageBoxButton.OK); 
                 return;
             }
-            //vòng lặp lấy các phần tử được chọn trên giao diện 
             while (lstDuLieu.SelectedItems.Count > 0)
             {
-                //lấy phần tử đầu tiên ra 
                 int data = (int)lstDuLieu.SelectedItems[0];
-                //xóa khỏi danh sách 
                 dsDuLieu.Remove(data);
-                //xóa dữ liệu trên giao diện 
                 lstDuLieu.Items.Remove(data);
             }
         }
-        private void BtnXoaToanBoPhanTu_Click(object sender,
-                                     RoutedEventArgs e)
+        private void BtnXoaToanBoPhanTu_Click(object sender,RoutedEventArgs e)
         {
-            dsDuLieu.Clear();//xóa toàn bộ dữ liệu  
+            dsDuLieu.Clear();  
             HienThiDanhDanh();
         }
     }
